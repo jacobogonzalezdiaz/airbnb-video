9b4b350b42bd9125397e7c2e0f6a379f3a4a402c
jacobogonzalezdiaz <jacobo_gonzalez@msn.com>
2023-08-06 23:10:56 +0100

    video-6

--- app/components/modals/LoginModal.tsx
+++ app/components/modals/LoginModal.tsx
@@ -89,13 +89,13 @@ const LoginModal = () => {
              outline
              label='Continua con Google'
              icon={FcGoogle}
-             onClick={() =>{}}
+             onClick={() =>signIn('google')}
              />
               <Button
              outline
              label='Continua con Github'
              icon={AiFillGithub}
-             onClick={() =>{}}
+             onClick={() =>signIn('github')}
              />
              <div className='text-neutral-500 text-center mt-4 font-light'>
                 <div className=' justify-center flex flex-row items-center gap-2'>
